edd2d0d972764b9718b1fca84da875e179a1545a
Jonathon Hill <jcgh582@gmail.com>
2018-10-06 15:22:39 -0700

    [Jono] console log what bloom sends us

--- routes/bloom.js
+++ routes/bloom.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const transformRequestIntoModel = req => ({
+  bloomId: req.body.bloom_id,
+  data: req.body.data,
+});
+
 router.post('/', function(req, res, next) {
-  res.send(201);
+  const data = transformRequestIntoModel(req);
+
+  console.log(data);
+
+  res.sendStatus(201);
 });
 
 module.exports = router;
